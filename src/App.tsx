dbe69f4023988aefa5eaa9c6ead6dbe2d24100ee
SeryVathana <yooseryvathana@gmail.com>
2024-04-30 21:00:48 +0700

    first-day

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div className="flex justify-center h-[100vh] items-center">
+      <Button>Hello world</Button>
+    </div>
+  );
+}
